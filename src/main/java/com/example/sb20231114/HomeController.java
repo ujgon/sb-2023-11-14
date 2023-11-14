03fc9e5123286892c8cde5c24bd632ba7ef26be2
Yoo <ujgon@naver.com>
2023-11-14 10:45:59 +0900

    calc

--- src/main/java/com/example/sb20231114/HomeController.java
+++ src/main/java/com/example/sb20231114/HomeController.java
@@ -13,4 +13,23 @@ public class HomeController {
         System.out.println("안녕하세요!!!");
         return "안녕하세요.";
     }
+
+    @GetMapping("/about")
+    @ResponseBody    //리턴값을 고객에게 보이시오(브라우저에서)
+    String showAbout() {
+        return "개발자 커뮤니티.";
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody    //리턴값을 고객에게 보이시오(브라우저에서)
+    String showCalc(int a, int b) {
+        return "계산 결과 : %d".formatted(a + b);
+    }
+
+    @GetMapping("/calc2")
+    @ResponseBody    //리턴값을 고객에게 보이시오(브라우저에서)
+    String showCalc2(Integer a, Integer b) {
+        return "a : " + a + ", b :" + b;
+    }
 }
+
